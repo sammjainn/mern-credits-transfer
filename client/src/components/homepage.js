408a1d7afae6f8f38759d446bdc38c291e4cba64
samjain123 <i_samriddhi.j@olcademy.in>
2020-09-27 15:14:07 +0530

    proxy added

--- client/src/components/homepage.js
+++ client/src/components/homepage.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 export default function homepage() {
   return (
     <div>
-      <div class='jumbotron my-4'>
-        <h1 class='display-4'>Welcome!</h1>
-        <p class='lead'>This is a simple credit transfer app.</p>
-        <hr class='my-4' />
+      <div className='jumbotron my-4'>
+        <h1 className='display-4'>Welcome!</h1>
+        <p className='lead'>This is a simple credit transfer app.</p>
+        <hr className='my-4' />
         <p>Made by Samriddhi Jain.</p>
         <Link to={'/users'} className='btn btn-primary btn-lg'>
           Get started
